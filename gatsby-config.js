185ef41c813e1b25083b77613c47e779a06cece0
andy <andy@podotree.com>
2019-05-13 01:24:56 +0900

    global css, typography.js 적용

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,4 +6,19 @@
 
 module.exports = {
   /* Your site config here */
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,13 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
+    <h1>fake it till you make it</h1>
+    <p>이번에는 진짜로 만든다~</p>
+    <p>
+      typography.js 의 테마는 github 테마를 사용했음. <br />
+      한글을 잘 사용할 수 있는 테마가 있으면 좋겠는데, 잘 모르겠다.
+    </p>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+  </div>
+)
