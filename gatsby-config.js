1cdf5338cb148455d359b9cdeb1b3aee56c9fa04
andy <andy@podotree.com>
2019-05-18 18:24:24 +0900

    google analytics 설정 수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,20 +44,6 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: "UA-140419734-1",
-        head: false,
-        anonymize: true,
-        respectDNT: true,
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Enables Google Optimize using your container Id
-        optimizeId: "GTM-M6GCRCV",
-        // Enables Google Optimize Experiment ID
-        //experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
-        // Set Variation ID. 0 for original 1,2,3....
-        //variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-        // Any additional create only fields (optional)
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "gyus.me",
       },
     },
   ],
