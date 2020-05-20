3b07ab59e6c97ec7dfcd30278911c74589bafa81
andy.sg <andy@kakaopage.com>
2020-05-21 01:23:59 +0900

    SSO 설정 수정

--- src/components/seo.js
+++ src/components/seo.js
@@ -52,11 +52,13 @@ const SEO = ({ path, description, image: metaImage, title }) => {
       <meta property="og:description" content={description} />
       {image && <meta property="og:image" content={image} />}
 
-      <meta name="twitter:card" content="summary" />
+      <meta property="twitter:card" content="summary" />
       <meta name="twitter:creator" content={defaults.twitter} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
+      <meta name="twitter:url" content={url} />
       {image && <meta name="twitter:image" content={image} />}
+      <meta name="author" content="seungkyoo.park@gmail.com" />
     </Helmet>
   )
 }
